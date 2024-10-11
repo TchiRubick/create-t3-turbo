17ec3801e0473a43f3b0f59aee263bccff93e7fb
Tchi <tchi.devica@gmail.com>
2024-10-12 00:12:12 +0300

    chore: lending page

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,44 +1,23 @@
-import { Suspense } from "react";
+import { CardsPrice } from "./_components/cards-price";
+import { CustomersService } from "./_components/customers-service";
+import { Footer } from "./_components/footer";
+import { HeroSection } from "./_components/Hero-section";
+import { NavBar } from "./_components/nav-bar";
+import { OtherMenus } from "./_components/other-menus";
 
-import { api, HydrateClient } from "~/trpc/server";
-import { AuthShowcase } from "./_components/auth-showcase";
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "./_components/posts";
-
-export const runtime = "edge";
-
-export default function HomePage() {
-  // You can await this here if you don't want to show Suspense fallback below
-  void api.post.all.prefetch();
-
+const HomePage = () => {
   return (
-    <HydrateClient>
-      <main className="container h-screen py-16">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-primary">T3</span> Turbo
-          </h1>
-          <AuthShowcase />
-
-          <CreatePostForm />
-          <div className="w-full max-w-2xl overflow-y-scroll">
-            <Suspense
-              fallback={
-                <div className="flex w-full flex-col gap-4">
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                </div>
-              }
-            >
-              <PostList />
-            </Suspense>
-          </div>
-        </div>
+    <div className="flex min-h-screen flex-col bg-blue-950">
+      <NavBar />
+      <main className="flex-1">
+        <HeroSection />
+        <OtherMenus />
+        <CustomersService />
+        <CardsPrice />
       </main>
-    </HydrateClient>
+      <Footer />
+    </div>
   );
-}
+};
+
+export default HomePage;
