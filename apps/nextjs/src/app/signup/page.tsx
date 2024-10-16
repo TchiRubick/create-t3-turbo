0dbe5ff691d95e8b1a03088e34bb7f6621f5d439
Tchi <tchi.devica@gmail.com>
2024-10-16 18:18:41 +0300

    chore: login and register page

--- apps/nextjs/src/app/signup/page.tsx
+++ apps/nextjs/src/app/signup/page.tsx
@@ -1,21 +1,61 @@
-import { signup } from "@acme/lucia/actions";
+import Link from "next/link";
 
-export default function Page() {
-  return (
-    <>
-      <h1>Create an account</h1>
-      <form action={signup}>
-        <label htmlFor="username">Username</label>
-        <input name="username" id="username" />
-        <br />
-        <label htmlFor="email">Email adress</label>
-        <input name="email" id="email" />
-        <br />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <br />
-        <button>Continue</button>
-      </form>
-    </>
-  );
-}
+import { signup } from "@acme/lucia/actions";
+import { Button } from "@acme/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@acme/ui/card";
+import { Input } from "@acme/ui/input";
+import { Label } from "@acme/ui/label";
+
+const SignupPage = () => (
+  <div className="flex min-h-screen flex-col justify-center">
+    <form action={signup} className="mx-auto max-w-sm">
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="username">User name</Label>
+              <Input id="username" name="username" placeholder="Max" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" name="password" type="password" />
+            </div>
+            <Button type="submit" className="w-full">
+              Create an account
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already have an account?
+            <Link href="/signin" className="underline">
+              Sign in
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </form>
+  </div>
+);
+
+export default SignupPage;
